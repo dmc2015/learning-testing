4a9a7835b2904e2af731cf22abcd40aceb383c25
dmc2015 <mclamb.donald@gmail.com>
2018-05-12 14:15:50 -0400

    getting started, vid4, structured project

---
+++ spec/card_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+require 'card'
+
+RSpec.describe 'a playing card' do
+
+  it 'has a suit' do
+    raise unless Card.new(suit: :spades, rank: 4).suit == :spades
+  end
+
+  it 'needs a rank' do
+    raise unless Card.new(suit: :spades, rank: 4).rank == 4
+  end
+
+  describe 'a jack' do
+    it 'ranks higher than a 10' do
+      lower = Card.new(suit: :spades, rank: 10)
+      higher = Card.new(suit: :spades, rank: :jack)
+      raise if higher.rank < lower.rank
+    end
+    it 'ranks at as 11' do
+      raise unless Card.new(suit: :spades, rank: :jack).rank == 11
+    end
+  end
+
+  describe 'a queen' do
+    it 'ranks higher than a 11' do
+      lower = Card.new(suit: :spades, rank: 11)
+      higher = Card.new(suit: :spades, rank: :queen)
+      raise if higher.rank < lower.rank
+    end
+    it 'ranks at as 11' do
+      raise unless Card.new(suit: :spades, rank: :queen).rank == 12
+    end
+  end
+
+  describe 'a king' do
+    it 'ranks higher than a 12' do
+      lower = Card.new(suit: :spades, rank: 12)
+      higher = Card.new(suit: :spades, rank: :king)
+      raise if higher.rank < lower.rank
+    end
+    it 'ranks at as 11' do
+      raise unless Card.new(suit: :spades, rank: :king).rank == 13
+    end
+  end
+
+end
